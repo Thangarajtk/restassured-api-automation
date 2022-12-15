35a59b0c2a7e413512c81bac4a8127e6bd636d18
Thangaraj <thangarajatk@gmail.com>
2022-12-15 18:21:36 +0530

    Added maven-checkstyle-plugin

--- src/test/java/com/automation/practice/DownloadFileTest.java
+++ src/test/java/com/automation/practice/DownloadFileTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.io.*;
 
+import static com.automation.constants.FrameworkConstants.RESOURCES_FOLDER_PATH;
 import static io.restassured.RestAssured.given;
 
 public final class DownloadFileTest {
@@ -11,15 +12,15 @@ public final class DownloadFileTest {
     @Test
     public void testDownloadFile() throws IOException {
         InputStream is = given().
-                baseUri("https://raw.githubusercontent.com").
+                baseUri("https://github.com").
                 log().all().
                 when().
-                get("/appium/appium/master/sample-code/apps/ApiDemos-debug.apk").
+                get("/appium/java-client/blob/master/src/test/resources/apps/ApiDemos-debug.apk").
                 then().
                 log().all().
                 extract().asInputStream();
 
-        OutputStream os = new FileOutputStream("ApiDemos-debug.apk");
+        OutputStream os = new FileOutputStream(RESOURCES_FOLDER_PATH + "ApiDemos-debug.apk");
         byte[] bytes = new byte[is.available()];
         is.read(bytes);
         os.write(bytes);
