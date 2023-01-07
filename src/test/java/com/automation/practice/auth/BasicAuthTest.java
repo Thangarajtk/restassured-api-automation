ad1a2859d1f60c17c777905c94eb1f2c8082d519
Thangaraj <thangarajatk@gmail.com>
2023-01-07 13:41:23 +0530

    Added logger with log4j2

--- src/test/java/com/automation/practice/auth/BasicAuthTest.java
+++ src/test/java/com/automation/practice/auth/BasicAuthTest.java
@@ -3,12 +3,16 @@ package com.automation.practice.auth;
 import io.restassured.response.Response;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class BasicAuthTest {
+
+  private static final Logger LOG = getLogger();
 
   @Test
   public void basicAuth() {
@@ -19,5 +23,6 @@ public final class BasicAuthTest {
       .get("https://postman-echo.com/basic-auth");
 
     response.prettyPrint();
+    LOG.debug(response);
   }
 }
